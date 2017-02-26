6aa9a2cb43092e9b104ddcb66e899a8ab7d1fe15
rafael <derkach.rafael@gmail.com>
2017-02-26 11:52:23 +0200

    add validation and company page

--- js/script.js
+++ js/script.js
@@ -1,38 +1,121 @@
-$(function() {
-    // LOADER
-$(window).load(function() {
-    setTimeout(function() {
-      $('.loader').fadeOut('slow', function() {});
-    }, 1500);
-  });
-
-
+$(function(){
 // password toggle
-var passwordToggle = document.querySelector('.js-password-toggle');
+var passwordToggle = document.querySelector('.jsPasswordToggle');
 passwordToggle.addEventListener('change', function() {
-  var password = document.querySelector('.js-password'),
-    passwordLabel = document.querySelector('.js-password-txt');
+  var password = document.querySelector('.jsPassword'),
+  passwordLabel = document.querySelector('.jsPasswordTxt');
   if (password.type === 'password') {
-    password.type = 'text';
-    passwordLabel.innerHTML = 'Hide';
+	 password.type = 'text';
+	 passwordLabel.innerHTML = 'Hide';
   } else {
-    password.type = 'password';
-    passwordLabel.innerHTML = 'Show';
+	 password.type = 'password';
+	 passwordLabel.innerHTML = 'Show';
   }
   password.focus()
 });
 // change icon select gender
-var selectGender = document.querySelector(".select-gender");
-    selectGender.addEventListener("change", toggleIcon);
-    function toggleIcon(){
-
-    }
-
-    //SVG Fallback
-    if (!Modernizr.svg) {
-        $("img[src*='svg']").attr("src", function() {
-            return $(this).attr("src").replace(".svg", ".png");
-        });
-    };
+var selectGender = document.querySelector(".genderSelect");
+selectGender.addEventListener("change", toggleIcon);
+function toggleIcon(){
+  var selected = selectGender.options[selectGender.selectedIndex].value,
+  iconMale = document.querySelector("span .icon");
+  if(selected == "female"){
+	 iconMale.classList.remove("fa-male");
+	 iconMale.classList.add("fa-female");
+  }
+  else if (selected == "male"){
+	 iconMale.classList.remove("fa-female");
+	 iconMale.classList.add("fa-male");
+  }
+};
+// form validation
+var bsValidator = $('.loginForm').bootstrapValidator({
+  feedbackIcons: {
+	 validating: 'glyphicon glyphicon-refresh'
+  },
+  excluded: [':disabled'],
+  fields: {
+	 name: {
+		validators: {
+		  stringLength: {
+			 min: 2,
+			 max: 45,
+		  },
+		  notEmpty: {
+			 message: 'Please supply your first name'
+		  }
+		}
+	 },
+	 secondName: {
+		validators: {
+		 stringLength: {
+		  min: 2,
+		  max: 45,
+		},
+		notEmpty: {
+		  message: 'Please supply your last name'
+		}
+	 }
+  },
+  email: {
+	 validators: {
+		notEmpty: {
+		  message: 'Please supply your email address'
+		},
+		emailAddress: {
+		  message: 'Please supply a valid email address'
+		}
+	 }
+  },
+  password: {
+	 validators: {
+	  stringLength: {
+		min: 2,
+		max: 15,
+	 },
+	 notEmpty: {
+		message: 'Please supply your password'
+	 }
+  }
+},
+terms: {
+  validators: {
+	 notEmpty: {
+		message: 'Please supply a description of your project'
+	 }
+  }
+}
+}
 });
+bsValidator.on('success.form.bv', function(e) {
+ e.preventDefault();
+				// $('.confirmBlock').slideDown({ opacity: "show" }, "slow");
+				// $('.loginFrom').data('bootstrapValidator').resetForm();
+				// Get the form instance
+				var $form = $(e.target);
+				// Get the BootstrapValidator instance
+				var bv = $form.data('bootstrapValidator');
+window.location.replace("company.html");
+				// Use Ajax to submit form data
+				// $.post($form.attr('action'), $form.serialize(), function(result) {
+				//     console.log(result);
+				// }, 'json');
+				// $.ajax({
+				//   type: "POST",
+				//   url: "mail.php",
+				//   success : function(){
+				// 	 if (bsValidator === true){
+				// 		alert("hello")
+				// 	 } else {
+				// 	  alert("false")
+				// 	}
+				//  }
+			 //  });
+
+
+
+			 });
+});
+
+
 
