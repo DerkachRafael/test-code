827b8b555cbdbacd6dffd288e05e57e2699d9a22
rafael <derkach.rafael@gmail.com>
2017-02-27 08:39:09 +0200

    get list of companies

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ App.prototype = Object.create(Helper.prototype);
 
 App.prototype.init = function(){
 	new TotalCompany();
+	// new InfoSlider();
 };
 
 window.addEventListener("DOMContentLoaded", function(){
--- js/modules/totalCompanies.js
+++ js/modules/totalCompanies.js
@@ -1,7 +1,8 @@
 function TotalCompany(){
 	this.getXHR();
+	// s
+	
 }
-
 TotalCompany.prototype = Object.create(App.prototype);
 
 TotalCompany.prototype.getXHR = function(){
@@ -12,11 +13,43 @@ TotalCompany.prototype.resultXHR = function(obj){
 		console.log(data);
 		totalStr = data.list.length;
 		console.log(totalStr);
-		contentTotalCompany = document.querySelector(".contentTotalCompany");
+		contentTotalCompany = document.querySelector(".contentTotalCompany"),
+		listTemplate = document.querySelector("#listTemplate").innerHTML,
+		compiledTemplate = Handlebars.compile(listTemplate),
+		generateHTML = compiledTemplate(data);
+		containerList = document.querySelector(".listCompany"),
+		containerList.innerHTML = generateHTML;
+		console.log(compiledTemplate);
+		console.log(generateHTML);
 		contentTotalCompany.innerHTML = totalStr;
 		console.log(contentTotalCompany);
+		this.nameCompany = document.querySelector(".nameCompany");
+		console.log(this.nameCompany);
+		for(var i = 0; i < this.nameCompany.length; i++){ 
+			console.log("for+");
+		this.nameCompany[i].addEventListener("click", this.activateItem.bind(this));
+	}
 }
-TotalCompany.prototype.templateList = function(data){
-	var list = JSON.parse(obj);
-	console.log(list);
+TotalCompany.prototype.clickHandler = function(){
+	
 }
+TotalCompany.prototype.activateItem = function(){
+	console.log("click+");
+	console.log(this);
+	$(this).addClass("activeName");
+}
+
+// var nameCompany = document.querySelectorAll(".nameCompany").length;
+// console.log(nameCompany);
+// for(var i = 0; i < nameCompany.length; i++){ 
+// 	nameCompany[i].addEventListener("click", activeCompany);
+// }
+// function activeCompany(){
+// 	this.classList.add("activeName");
+// }
+
+
+
+
+
+
